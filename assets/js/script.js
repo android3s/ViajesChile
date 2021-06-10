5a2d676b3885abd0eb106d5c6cb9a551b7aace9d
Andres <asilva0545@e-camp.cl>
2021-06-09 23:05:59 -0400

    Configuración de links de rrss Arreglo de animación de avion de footer Adicion de tooltips y hover para rrss smoothscroll para links de navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,36 @@
-$("#footer_text").mouseover(function () {
-    $("#footer_icon").removeAttr("style")
-    $("#footer_icon").animate({"padding-left": "+=" + $("#footer_icon").parent().width() + "px"}, 2000);
-
-    $("#footer_icon").removeAttr("style")
-    $("#footer_icon").attr("display","none")
-
+//Tooltips
+$(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip()
 })
+//Animación AVION
+let tmp=0
+$("#footer_text").mouseover(function () {
+    if(tmp == 0) {
+        tmp = 1
+        $("#footer_icon").removeAttr("style")
+        $("#footer_icon").animate({"padding-left": "+=" + ($("#footer_icon").parent().width()/2) + "px"}, 2000, null, function(){tmp = 0});
+
+        $("#footer_icon").removeAttr("style")
+        $("#footer_icon").attr("display", "none")
+    }
+})
+//SmoothScroll
+$(".scroll-hash").click(function (event){
+    event.preventDefault();
+
+    var url=this.hash;
+
+    $("html").animate(
+        {
+            scrollTop: $(url).offset().top - 50,
+        },200
+    )
+})
+//Hover para los iconos de las RRSS
+$( ".rrss-icons" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+});
 //Typeform - Formulario
 let qs, js, q, s, d = document,
     gi = d.getElementById,
